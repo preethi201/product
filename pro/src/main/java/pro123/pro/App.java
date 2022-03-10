1eb5c74063f5195a83ae417181b8b3a469b66d8d
preethi  N S <292849@ConneqtCorp.com>
2022-03-10 13:53:24 +0530

    creat new account

---
+++ pro/src/main/java/pro123/pro/App.java
@@ -0,0 +1,13 @@
+package pro123.pro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
